220b24b5a7c325d6824e105a088ca2ea0ffa7006
Ruijie Gao <ruijiegao@Ruijies-MacBook-Pro.local>
2025-09-22 17:06:40 -0400

    Add initial implementation of __mulhu64_soft for 64-bit unsigned multiplication

---
+++ softmul/include/soft_mul.h
@@ -0,0 +1,14 @@
+#ifndef SOFT_MUL_H
+#define SOFT_MUL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned long long __mulhu64_soft(unsigned long long a, unsigned long long b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SOFT_MUL_H */
